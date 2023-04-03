c8fce63be2323e7eb7e5deb211d89023b3a7e460
GitHub Action <action@github.com>
2023-04-03 13:32:13 +0000

    Template cleanup

--- src/main/kotlin/com/github/longyg/docxbrowser/services/MyProjectService.kt
+++ src/main/kotlin/com/github/longyg/docxbrowser/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.longyg.docxbrowser.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.longyg.docxbrowser.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
